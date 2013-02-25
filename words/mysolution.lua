4241ad352bcd7ced25d0db1398096fd2ca90fce8
Thorben Krüger <github@benthor.name>
2013-02-25 18:37:23 +0100

    now with more functional programming inside

--- words/mysolution.lua
+++ words/mysolution.lua
@@ -1,50 +1,70 @@
 -- io module required for reading from stdin
 require 'io'
 
--- hashtable where we keep the frequencies in
-freqs = {}
 
--- define metatable that returns 0 instead of nil when its associated table
--- is accessed with a non-existing key
-mt = { __index = function(t,word) return 0 end }
+function get_frequencies(iterator) 
+    -- hashtable where we keep the frequencies in
+    local freqs = {}
 
--- assign the metatable to our frequency table, the freqs table will now
--- return 0 instead of nil if a non-existing element is accessed
-setmetatable(freqs, mt)
+    -- define metatable that returns 0 instead of nil when its associated table
+    -- is accessed with a non-existing key
+    mt = { __index = function(t,word) return 0 end }
 
--- get the freqencies
--- io.lines() without argument just reads from stdin
-for line in io.lines() do
-    -- lowercase the line, then return match iterator for only "lower case word
-    -- characters" and iterate
-    for word in string.gmatch(string.lower(line), "%l+") do
-        -- thanks to our metatable, an access to non-existing freqs[word] entry
-        -- just returns 0, so the following just works(TM)
-        freqs[word] = freqs[word] + 1
+    -- assign the metatable to our frequency table, the freqs table will now
+    -- return 0 instead of nil if a non-existing element is accessed
+    setmetatable(freqs, mt)
+
+    -- get the freqencies
+    for line in iterator() do
+        -- lowercase the line, then return match iterator for only "lower case word
+        -- characters" and iterate
+        for word in string.gmatch(string.lower(line), "%l+") do
+            -- thanks to our metatable, an access to non-existing freqs[word] entry
+            -- just returns 0, so the following just works(TM)
+            freqs[word] = freqs[word] + 1
+        end
+    end
+    return freqs
+end
+
+
+function mk_tuple_sequence(frequencies)
+    -- get a table to become a sequence of tuples
+    -- table layout: integers as indicies/keys, {word,frequency}-tuples as values
+    local result = {}
+
+    -- iterate over key/value pairs in the freqs table
+    for word,freq in pairs(frequencies) do
+        -- will insert {word,frequency} as value for next available integer index/key
+        -- i.e., inserting {"spam",5} into table {1={"foo",6}, 2={"bar",2}} will result in
+        -- {1={"foo",6}, 2={"bar",2}, 3={"spam",5}}
+        table.insert(result, {word,freq})
+    end
+    return result
+end
+
+-- is within the scope of an easy anonymous function but making this explicit anyway
+function tuple_comparator(tuple_a, tuple_b)
+    return tuple_a[2] > tuple_b[2]
+end
+
+
+function order_tuple_sequence(sequence, comparator)
+    -- unfortunately table.sort sorts in-place
+    table.sort(sequence, comparator)
+    return sequence
+end
+
+
+function print_first_n(sequence, n)
+    if n ~= 0 then
+        -- doing head recursion here, works as expected
+        print_first_n(sequence, n-1)
+        print(string.format("%s: %i", sequence[n][1], sequence[n][2]))
     end
 end
 
--- get a table to become a sorted list of frequent words
--- table layout: integers as indicies/keys, words as values
-sorted = {}
-
--- iterate over key/value pairs in the freqs table, only the keys (words) are
--- of interest so we use "dummy" variable "_" for the values
-for word,_ in pairs(freqs) do
-    -- will insert word as value for next available integer index/key
-    -- i.e., inserting "spam" into table {1="foo", 2="bar"} will result in
-    -- {1="foo", 2="bar", 3="spam"}
-    table.insert(sorted, word)
-end
-
--- reorder the elements in the table using an anonymous compare function that
--- looks up the word frequencies in the freqs table. reorder from highest
--- associated freq to lowest associated freq. NOTE: this happens in-place
-table.sort(sorted, (function(a,b) return freqs[a] > freqs[b] end))
-
--- finally print the top 10 words, look up their freqencies in the freqs table
-for i=1,10 do
-    word = sorted[i]
-    print(string.format("%s: %i", word, freqs[word]))
-end
+-- io.lines() without argument just reads from stdin, although we pass this as a function here
+print_first_n(order_tuple_sequence(mk_tuple_sequence(get_frequencies(io.lines)), tuple_comparator), 10)
+                
 
