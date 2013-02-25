8f6e19aebe8c292ca66bcd75198362fa8a5ec3ef
Thorben Krüger <github@benthor.name>
2013-02-25 22:13:26 +0100

    cleaned up code

--- words/mysolution.py
+++ words/mysolution.py
@@ -5,6 +5,7 @@ def yieldwords():
     matcher = compile("[a-z]*")
     for line in stdin.readlines():
         for word in filter(lambda x: x != '', [m.group(0) for m in matcher.finditer(line.lower())]):
+            # yield is completely sufficient, we don't need to create something like an explicit result list
             yield word
 
 def count(iterator):
@@ -14,7 +15,8 @@ def count(iterator):
     return d
 
 def rank(d):
-    return sorted([(x,d[x]) for x in d.keys()], key=lambda y: d[y[0]], reverse=True)
+    # get tuples from dictionary and sort by second element in each tuple
+    return sorted([x for x in d.items()], key=lambda y: y[1], reverse=True)
 
 [print("%s: %i" % (tup[0],tup[1])) for tup in rank(count(yieldwords))[:10]]
 
