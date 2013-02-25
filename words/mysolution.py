83f9b8463a46fbfa555cb662c5410dc35d1fccae
Thorben Krüger <github@benthor.name>
2013-02-25 22:17:42 +0100

    even further code slimming/readablity

--- words/mysolution.py
+++ words/mysolution.py
@@ -18,7 +18,7 @@ def rank(d):
     # get tuples from dictionary and sort by second element in each tuple
     return sorted(d.items(), key=lambda y: y[1], reverse=True)
 
-[print("%s: %i" % (tup[0],tup[1])) for tup in rank(count(yieldwords))[:10]]
+[print("%s: %i" % tupel) for tupel in rank(count(yieldwords))[:10]]
 
 
 
