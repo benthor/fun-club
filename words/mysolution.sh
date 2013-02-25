fcddbf17155794c283fc64ec0dbd27be30f699be
Thorben Krüger <github@benthor.name>
2013-02-25 17:17:59 +0100

    nicely commented simple bash one-liner (alternative solution)

--- words/mysolution.sh
+++ words/mysolution.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-sed -e 's/.*/\L\0/g' | grep -oE "[a-z]*" | sort | uniq -c | sort -rn | head
+
+# 1. reads something from stdin, converts to lower case
+# 2. returns as many consecutive lowercase alphabetic characters at a time as possible ("greedy")
+# 3. sorts the resulting strings alphabetically
+# 4. displays only one instance of each redundant line, prefixed by the amount of lines originally seen
+# 5. sorts again, this time numerically (i.e., according to prefixed count) and reversed
+# 6. head gives only the first 10 resulting lines
+# 7. (optional) awk de-pretty-fies the output in favour of emulating the example solution output
+
+sed -e 's/.*/\L\0/g' | grep -oE "[a-z]*" | sort | uniq -c | sort -rn | head | awk '{ print $2 ": " $1 }'
